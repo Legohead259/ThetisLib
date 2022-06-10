93f106e64ee09a0666a284abb6c339a1699b3096
Braidan <legohead259@gmail.com>
2022-06-10 08:22:46 -0400

    Added the telemetry data packet definition

--- src/ThetisLib.h
+++ src/ThetisLib.h
@@ -5,6 +5,51 @@
 #include <Arduino.h>
 
 
+// =========================
+// === GENERAL FUNCTIONS ===
+// =========================
+
+
+struct Telemetry {
+    char timestamp[32];         // Timestamp in UTC obtained from GPS satellites
+    float voltage;              // Battery voltage in V
+    bool GPSFix;                // If GPS has positive fix on location
+    uint8_t numSats;            // Number of satellites GPS is communicating with
+    uint8_t HDOP;               // Accuracy of GPS reading. Lower is better. In tenths (divide by 10. when displaying)
+    long latitude;              // In millionths of a degree (divide by 1000000. when displaying)
+    long longitude;             // In millionths of a degree (divide by 1000000. when displaying)
+    long GPSSpeed;              // In thousandths of a knot (divide by 1000. when displaying)
+    long GPSCourse;             // In thousandths of a degree (divide by 1000. when displaying)
+    uint8_t sysCal = 0;         // IMU system calibration, 0-3 with 3 being fully calibrated
+    uint8_t gyroCal = 0;        // IMU gyroscope calibration, 0-3 with 3 being fully calibrated
+    uint8_t accelCal = 0;       // IMU accelerometer calibration, 0-3 with 3 being fully calibrated
+    uint8_t magCal = 0;         // IMU magnetometer calibration, 0-3 with 3 being fully calibrated
+    float accelX;               // m/s^2
+    float accelY;               // m/s^2
+    float accelZ;               // m/s^2
+    float gyroX;                // rad/s
+    float gyroY;                // rad/s
+    float gyroZ;                // rad/s
+    float magX;                 // mGauss
+    float magY;                 // mGauss
+    float magZ;                 // mGauss
+    float roll;                 // degrees
+    float pitch;                // degrees
+    float yaw;                  // degrees
+    float linAccelX;            // m/s^2
+    float linAccelY;            // m/s^2
+    float linAccelZ;            // m/s^2
+    float quatW;                //
+    float quatX;                //
+    float quatY;                //
+    float quatZ;                //
+    float imuTemp;              // °Celsius from the IMU
+    uint8_t state;              // State reported by the package.
+    uint8_t packetSize;         // The size of the telemetry packet. Used as a debug tool for ground station/thetis comms.
+};
+Telemetry data;
+
+
 // ========================
 // === BNO055 FUNCTIONS ===
 // ========================
@@ -168,4 +213,4 @@ extern bool isLogging;
 
 void IRAM_ATTR logButtonISR();
 
-#endif
+#endif // THETISLIB_H
