d931ff0fdd1f1b6bd2b5e3475e95a872d56168d3
Braidan <legohead259@gmail.com>
2022-07-22 15:08:46 -0400

    Trying to fix some integration bugs

--- src/radios/Thetis_WiFi.h
+++ src/radios/Thetis_WiFi.h
@@ -1,9 +1,16 @@
 #ifndef THETIS_WIFIH
 #define THETIS_WIFIH
+
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
+#include <SPIFFS.h>
 
+// Configuration Data
+uint8_t deviceID;
+const char FW_VERSION[] = "0.4.0";
+const char HW_REVISION[] = "Rev F4";
 char ssid[32];
 char password[32];
+
 AsyncWebServer server(80); // Create AsyncWebServer object on port 80
 #endif // THETIS_WIFIH
