1cbe6907cbd9c89f2346249d3b33c097bf65370c
Legohead259 <legohead259@gmail.com>
2022-10-04 11:07:20 -0400

    Added some more state functions

--- src/states.h
+++ src/states.h
@@ -17,4 +17,40 @@ typedef enum Status {
     READY_NO_GPS,
     READY_GPS,
     STANDBY
-} Status_t;
+} status_t;
+
+static void updateSystemState() {
+    bool _isIMUCalibrated = data.sysCal == 3 && data.accelCal == 3 && data.gyroCal == 3 && data.magCal == 3;
+
+    if (!_isIMUCalibrated && !data.GPSFix)                      data.state = STANDBY;
+    else if (_isIMUCalibrated && !data.GPSFix && !isLogging)    data.state = READY_NO_GPS;
+    else if (_isIMUCalibrated && data.GPSFix  && !isLogging)    data.state = READY_GPS;
+    else if (_isIMUCalibrated && !data.GPSFix && isLogging)     data.state = LOGGING_NO_GPS;
+    else if (_isIMUCalibrated && data.GPSFix  && isLogging)     data.state = LOGGING_GPS;
+}
+
+static void updateSystemLED() {
+    switch (data.state) {
+        case LOGGING_NO_GPS:
+            pixel.setPixelColor(0, BLUE); pixel.show(); // Glow solid blue
+            break;
+        case LOGGING_GPS:
+            pixel.setPixelColor(0, GREEN); pixel.show(); // Glow solid green
+            break;
+        case READY_NO_GPS:
+            pulseLED(BLUE); // Pulse blue
+            break;
+        case READY_GPS:
+            pulseLED(GREEN); // Pulse green
+            break;
+        case STANDBY:
+            pixel.setPixelColor(0, 255, 191, 0); pixel.show(); // Glow solid amber
+            break;
+        case BOOTING:
+            pulseLED(PURPLE); // Pulse purple
+            break;
+        default:
+            pixel.setPixelColor(0, RED); pixel.show(); // Turn off LED
+            break;
+    }
+}
