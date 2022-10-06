1e5ad18a6c13280527c1049094fca3a841e35485
Braidan <legohead259@gmail.com>
2022-10-05 23:15:53 -0400

    Added some basic Kalman parameters TODO: Add a Kalman filter for GPS data

--- src/radios/gps.h
+++ src/radios/gps.h
@@ -1,10 +1,18 @@
 #ifndef THETIS_GPS_H
 #define THETIS_GPS_H
 
-#include <utility/MicroNMEA.h>
+#include <MicroNMEA.h>
 
 #define GPS_SYNC_INTERVAL 1 // minutes
 #define GPS_TIMEOUT 5000 // ms
+
+// TODO: Implement Kalman filtering for GPS data
+
+#define r_lat 	1.5 // GPS latitude measurement noise (m - radius of error)
+#define r_lon 	1.5 // GPS longitude measurement noise (m - radius of error)
+#define r_sog 	1.0 // GPS SOG measurement noise (m/s)
+#define r_cog 	1.0 // GPS COG measurement noise (deg)
+#define q_gps 	0.5 // Process error - gps
 
 // TODO: Implement MTK3339 API
 // TODO: Make GPS object-oriented
