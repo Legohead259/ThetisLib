930be4ca5c110742218cb54f8ba4e8cad944c6bf
Braidan <legohead259@gmail.com>
2022-10-05 23:16:50 -0400

    Refactored `Serial` to standard nomenclature

--- src/radios/wifi.cpp
+++ src/radios/wifi.cpp
@@ -4,23 +4,23 @@ AsyncWebServer server(80); // Create AsyncWebServer object on port 80
 
 bool initWIFIAP() {
     #ifdef WIFI_DEBUG
-    Serial.print("Starting WiFi access point...");
+    DEBUG_SERIAL_PORT.print("Starting WiFi access point...");
     #endif
     sprintf(configData.ssid, "Thetis-%03u", configData.deviceID); // Format AP SSID based on Device ID
     if (!WiFi.softAP(configData.ssid, "")) { // Start the access point with the deviceID SSID and no password
         #ifdef WIFI_DEBUG
-        Serial.println("Failed to start access point!");
+        DEBUG_SERIAL_PORT.println("Failed to start access point!");
         #endif
         return false;
     }
     #ifdef WIFI_DEBUG
-    Serial.println("done!");
+    DEBUG_SERIAL_PORT.println("done!");
     #endif
 
     IPAddress IP = WiFi.softAPIP();
     #ifdef WIFI_DEBUG
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
+    DEBUG_SERIAL_PORT.print("AP IP address: ");
+    DEBUG_SERIAL_PORT.println(IP);
     #endif
 
     // Route for root / web page
@@ -38,21 +38,21 @@ bool initWIFIAP() {
 }
 
 String processor(const String &var) {
-    Serial.print(var); Serial.print(": ");
+    DEBUG_SERIAL_PORT.print(var); DEBUG_SERIAL_PORT.print(": ");
     if (var == "DEVICE_ID") {
         char _deviceIDStr[4];
         sprintf(_deviceIDStr, "%03u", configData.deviceID);
-        Serial.println(_deviceIDStr);
+        DEBUG_SERIAL_PORT.println(_deviceIDStr);
         return _deviceIDStr;
     }
     else if (var == "FW_VERSION") {
-        Serial.println(configData.FW_VERSION);
+        DEBUG_SERIAL_PORT.println(configData.FW_VERSION);
         return configData.FW_VERSION;
     }
     else if (var == "HW_REVISION") {
-        Serial.println(configData.HW_REVISION);
+        DEBUG_SERIAL_PORT.println(configData.HW_REVISION);
         return configData.HW_REVISION;
     }
-    Serial.println("Unknown handle");
+    DEBUG_SERIAL_PORT.println("Unknown handle");
     return var;
 }
