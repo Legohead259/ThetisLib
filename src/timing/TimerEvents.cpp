15cd2a4810ca0175ee3670086e941fbff477cb2c
Braidan <legohead259@gmail.com>
2023-06-28 16:22:44 -0400

    Added durations and `onFinish` callbacks to `TimerEvents`

--- src/timing/TimerEvents.cpp
+++ src/timing/TimerEvents.cpp
@@ -1,10 +1,12 @@
 #include "TimerEvents.h"
 
-TimerEvent::TimerEvent(const char* n, unsigned long p, TimerEventHandler cbPtr, bool e) {
+TimerEvent::TimerEvent(const char* n, unsigned long p, TimerEventHandler onExecute, bool e, long d, TimerEventHandler onFinish) {
     name = n;
     period = p;
-    callbackPtr = cbPtr;
+    onExecutePtr = onExecute;
     isEnabled = e;
+    duration = d;
+    onFinishPtr = onFinish;
 }
 
 TimerEventsClass::TimerEventsClass() {
@@ -17,6 +19,7 @@ void TimerEventsClass::add(TimerEvent* event) {
     }
     events[numberOfEvents] = event;
     timeouts[numberOfEvents] = millis();
+    durations[numberOfEvents] = millis();
     event->index = numberOfEvents;
     numberOfEvents++;
 }
@@ -30,8 +33,20 @@ void TimerEventsClass::tasks() {
     const unsigned long time = millis();
     for (int i=0; i<numberOfEvents; i++) {
         if (!events[i]->isEnabled) continue; // Check if the event is enabled, skip if not
+        
+        if (events[i]->duration != -1) {                    // Check if the event has a specified duration
+            if (events[i]->firstCalled == 0) {              // Check if the event has been called yet
+                events[i]->firstCalled = time;              // Set the called flag
+                durations[i] = time + events[i]->duration;  // Set a stop time for the event
+            }
+            if (time >= durations[i]) {                     // Check if the event duration has passed
+                events[i]->disable();                       // Disable the event
+                events[i]->firstCalled = 0;                 // Reset the called flag
+                events[i]->finish();                        // Call the events onFinish callback
+            }
+        }
 
-        if (time >= timeouts[i]) {
+        if (time >= timeouts[i]) { // Check if the event timeout has passed
             timeouts[i] = time + events[i]->period;
             events[i]->execute();
         }
--- src/timing/TimerEvents.h
+++ src/timing/TimerEvents.h
@@ -8,16 +8,22 @@ typedef std::function<void()> TimerEventHandler;
 class TimerEvent {
 
 public:
-    TimerEvent(const char* n, unsigned long p, TimerEventHandler cbPtr, bool e);
+    TimerEvent(const char* n, unsigned long p, TimerEventHandler onExecute, bool e, long d=-1, TimerEventHandler onFinish=nullptr);
 
+    TimerEvent();
     const char* name;
     unsigned long period;
-    TimerEventHandler callbackPtr = nullptr;
+    TimerEventHandler onExecutePtr = nullptr;
+    TimerEventHandler onFinishPtr = nullptr;
     bool isEnabled;
+    long duration;
+    unsigned long firstCalled = 0;
     int index = -1;
 
-    void execute() { if (callbackPtr != nullptr) callbackPtr(); }
-    void setEventCallback(TimerEventHandler cbPtr) { callbackPtr = cbPtr; }
+    void execute() { if (onExecutePtr != nullptr) onExecutePtr(); }
+    void finish() { if (onFinishPtr != nullptr) onFinishPtr(); }
+    void setEventCallback(TimerEventHandler cbPtr) { onExecutePtr = cbPtr; }
+    void setFinishCallback(TimerEventHandler cbPtr) { onFinishPtr = cbPtr; }
     void enable() { isEnabled = true; }
     void disable() { isEnabled = false; }
 };
@@ -38,7 +44,8 @@ private:
     static const int maxNumberOfEvents = 32;
     int numberOfEvents;
     unsigned long timeouts[maxNumberOfEvents];
-    TimerEvent* events[maxNumberOfEvents]; 
+    unsigned long durations[maxNumberOfEvents];
+    TimerEvent* events[maxNumberOfEvents];
 };
 
 extern TimerEventsClass timerEvents;
